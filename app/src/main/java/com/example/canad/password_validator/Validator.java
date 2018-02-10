d358428318be497cd38b36bfe1b3764cd94ba238
VaxChen <canadavaxchen@gmail.com>
2018-02-09 21:49:53 -0400

    stage 2

--- app/src/main/java/com/example/canad/password_validator/Validator.java
+++ app/src/main/java/com/example/canad/password_validator/Validator.java
@@ -11,12 +11,32 @@ public class Validator extends AppCompatActivity {
             ele[i] = input.charAt(i);
         int result = 0;
 
-        //check if the input password is longer than 8 characters
-        if (input.length() >= 8)
+        //check if the input password is longer than 8 characters and shorter than 16 character
+        if (input.length() >= 8&&input.length()<=16)
             result++;
 
         //check if the input password is not "password"(case insensitive)
         if (!input.equalsIgnoreCase("password"))
+            result++;
+
+        //check if the input password has at least one upper case letter
+        for(int i=0;i<16;i++){
+            if(Character.isUpperCase(ele[i])) {
+                result++;
+                break;
+            }
+        }
+
+        //check if the input password has at least one digit
+        for(int i=0;i<16;i++){
+            if(Character.isDigit(ele[i])) {
+                result++;
+                break;
+            }
+        }
+
+        //check if the input password doesn't have a digit as the first character
+        if(!Character.isDigit(ele[0]))
             result++;
 
         return result;
